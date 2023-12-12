c32a9d3f8c29a9e9e79799672b62c2dc170012a9
mekdeskebede <kebedemekdes289@gmail.com>
2023-12-12 11:47:14 +0300

    implement add doctor

--- Express-backend/router/doctor.js
+++ Express-backend/router/doctor.js
@@ -2,4 +2,4 @@ const express = require("express");
 const router = express.Router();
 const doctorContoller = require("../controllers/doctor");
 
-router.post("/", upload.single("foodImage"), doctorContoller.addDoctor);
+router.post("/", upload.single("photo"), doctorContoller.addDoctor);
