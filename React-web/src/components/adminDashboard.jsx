295826c1d9bc3472596c66ac9de19a17394cedec
fev <feventolla99@gmail.com>
2023-12-15 01:33:50 +0300

    pagination

--- React-web/src/components/adminDashboard.jsx
+++ React-web/src/components/adminDashboard.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import image1 from "../assets/hero-hosp.png";
 import edit from "../assets/edit.svg";
+import { RxDashboard } from "react-icons/rx";
 
 const Admindashboard = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
 
   const data = [
     {
@@ -103,6 +106,16 @@ const Admindashboard = () => {
     // Implement edit logic here
     console.log(`Edit card with id ${id}`);
   };
+
+  // Pagination logic
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentData = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
   return (
     <main className="bg-white text-black min-h-screen max-w-full">
       <div className="p-5">
@@ -116,7 +129,11 @@ const Admindashboard = () => {
         </div>
 
         <div className="flex flex-row gap-20 pt-10">
-          <div className="flex font-semibold text-[#C276F0] ">Dashboard</div>
+          <div className="flex flex-row gap-3">
+            <RxDashboard className="text-[#C276F0]" />
+
+            <div className="flex font-semibold text-[#C276F0]  ">Dashboard</div>
+          </div>
           <div className="flex flex-col w-full">
             <div className="text-2xl font-semibold pb-10">
               Hospitals information
@@ -138,7 +155,7 @@ const Admindashboard = () => {
               </div>
             </div>
             <div className="grid grid-cols-4 gap-4">
-              {filteredData.map((card) => (
+              {currentData.map((card) => (
                 <div
                   key={card.id}
                   className="bg-white p-4 border rounded shadow"
@@ -161,15 +178,32 @@ const Admindashboard = () => {
                     <p className="card-description">{card.description}</p>
                     <button
                       onClick={() => handleDelete(card.id)}
-                      className="delete-button"
+                      className="text-red-300 text-md"
                     >
-                      Delete
+                      Delete Hospital
                     </button>
                   </div>
                 </div>
               ))}
             </div>
           </div>
+        </div>
+        <div className="flex justify-center mt-4">
+          {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+            (page) => (
+              <button
+                key={page}
+                onClick={() => handlePageChange(page)}
+                className={`mx-2 px-4 py-2 ${
+                  currentPage === page
+                    ? "bg-[#C276F0] text-white"
+                    : "bg-gray-200"
+                } rounded`}
+              >
+                {page}
+              </button>
+            )
+          )}
         </div>
       </div>
     </main>
