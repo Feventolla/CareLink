9c1e4feea5bb334792275fa2ecdaf300512d6cfa
fev <feventolla99@gmail.com>
2023-12-15 01:16:27 +0300

    admin-dashboard

---
+++ React-web/src/components/adminDashboard.jsx
@@ -0,0 +1,179 @@
+import { useState } from "react";
+import image1 from "../assets/hero-hosp.png";
+import edit from "../assets/edit.svg";
+
+const Admindashboard = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const data = [
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+    {
+      id: 1,
+      title: "Card 1",
+      description: "This is the first card",
+      imageUrl: { image1 },
+    },
+  ];
+  const filteredData = data.filter((card) =>
+    card.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const handleDelete = (id) => {
+    // Implement delete logic here
+    console.log(`Delete card with id ${id}`);
+  };
+
+  const handleEdit = (id) => {
+    // Implement edit logic here
+    console.log(`Edit card with id ${id}`);
+  };
+  return (
+    <main className="bg-white text-black min-h-screen max-w-full">
+      <div className="p-5">
+        <div className="flex flex-row pt-10">
+          <div className="text-4xl font-bold">
+            <span className="text-[#C276F0]">Care</span>Link
+          </div>
+          <div className="flex items-center justify-center w-full text-3xl text-[#3E435D] font-semibold ">
+            Welcome, Dagim
+          </div>
+        </div>
+
+        <div className="flex flex-row gap-20 pt-10">
+          <div className="flex font-semibold text-[#C276F0] ">Dashboard</div>
+          <div className="flex flex-col w-full">
+            <div className="text-2xl font-semibold pb-10">
+              Hospitals information
+            </div>
+            <div className="flex flex-row justify-between w-[70em] ">
+              <div>
+                <button className="bg-[#C276F0] text-white font-bold py-2 px-14 rounded">
+                  Add Hospital
+                </button>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  className="p-2 w-96 mb-4 border rounded"
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-4 gap-4">
+              {filteredData.map((card) => (
+                <div
+                  key={card.id}
+                  className="bg-white p-4 border rounded shadow"
+                >
+                  <div className="flex flex-row gap-24">
+                    <img
+                      src={card.imageUrl.image1}
+                      alt="Card"
+                      className="card-image"
+                    />
+                    <div
+                      onClick={() => handleEdit(card.id)}
+                      className="edit-icon"
+                    >
+                      <img src={edit} alt="Card" className="card-image" />
+                    </div>
+                  </div>
+                  <div className="card-content">
+                    <h3 className="card-title">{card.title}</h3>
+                    <p className="card-description">{card.description}</p>
+                    <button
+                      onClick={() => handleDelete(card.id)}
+                      className="delete-button"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Admindashboard;
