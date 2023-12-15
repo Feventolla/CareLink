46738d3616a553628a9329e0452e42fc272e86fd
mekdeskebede <kebedemekdes289@gmail.com>
2023-12-15 10:54:58 +0300

    add doctors list page

---
+++ React-web/src/component/DoctorDetail/DoctorCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import DocImage from "../../assets/images/doctor/doc5.jpeg";
+import { BiEdit } from "react-icons/bi";
+
+const DoctorCard = ({ doctorData }) => {
+  return (
+    <div className="bg-white py-4 px-6 rounded-lg shadow-lg relative">
+      <BiEdit className="absolute right-4 top-4" size={22} />
+      <img
+        src={DocImage}
+        alt="Doctor"
+        className="w-24 h-20 object-cover mb-4 rounded-xl mx-auto"
+      />
+      <h4 className="font-bold mb-4">{doctorData.name}</h4>
+      <p className="text-gray-600 text-sm">{doctorData.description}</p>
+      <div className="flex justify-end mt-4">
+        <button className="text-red-500 text-sm">Delete Doctor</button>
+      </div>
+    </div>
+  );
+};
+
+export default DoctorCard;
---
+++ React-web/src/component/common/SideBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { RxDashboard } from "react-icons/rx";
+import { LuLogOut } from "react-icons/lu";
+
+const Sidebar = () => {
+  return (
+    <div className="bg-white h-screen fixed top-0 left-0 hidden sm:block">
+      <div className="flex flex-row absolute bottom-3 left-2 gap-2">
+        <LuLogOut color="#131313" className="mt-1" />
+        <p className="text-[#131313]">Log Out</p>
+      </div>
+
+      <div className="m-6">
+        <h1 className="text-3xl font-semibold text-[#C276F0]">
+          Care<span className="text-black">Link</span>
+        </h1>
+        <div className="flex flex-row gap-4 mt-12">
+          <RxDashboard color="#C276F0" size={18} />{" "}
+          <p className="text-sm text-[#C276F0]">DashBoard</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
