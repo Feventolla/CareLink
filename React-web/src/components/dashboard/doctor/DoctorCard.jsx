0240dca0d46a27703d0b20ec36f7aac339087bce
mekdeskebede <kebedemekdes289@gmail.com>
2023-12-16 20:06:17 +0300

    fix folder structure

--- React-web/src/components/dashboard/doctor/DoctorCard.jsx
+++ React-web/src/components/dashboard/doctor/DoctorCard.jsx
@@ -11,7 +11,7 @@ const DoctorCard = ({ doctorData }) => {
   return (
     <div className="bg-white py-4 px-6 rounded-lg shadow-lg relative">
       <BiEdit
-        className="absolute right-4 top-4"
+        className="absolute right-4 top-4 cursor-pointer"
         size={22}
         onClick={handleEditDoctor}
       />
@@ -23,7 +23,9 @@ const DoctorCard = ({ doctorData }) => {
       <h4 className="font-bold mb-4">{doctorData.name}</h4>
       <p className="text-gray-600 text-sm">{doctorData.description}</p>
       <div className="flex justify-end mt-4">
-        <button className="text-red-500 text-sm">Delete Doctor</button>
+        <button className="text-red-500 text-sm cursor-pointer">
+          Delete Doctor
+        </button>
       </div>
     </div>
   );
